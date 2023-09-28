35fb4074e5b28b48fef64f2ac3587dbad03a5fb1
Kayque Moraes <kaymoraesdias@gmail.com>
2023-09-28 17:54:09 -0300

    adicionando mais getters

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -32,6 +32,22 @@ public class Filme {
         return totalAvaliacoes;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
     //Setters
     public void setNome(String nome) {
         this.nome = nome;
